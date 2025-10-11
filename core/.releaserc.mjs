8956a499c7ecafc1ebcf82a2e51b7a7059d4da28
Ziv Perry <ziv.perry@zoominfo.com>
2025-10-11 20:09:41 +0300

    feat(core): update release script

--- core/.releaserc.mjs
+++ core/.releaserc.mjs
@@ -8,7 +8,7 @@ export default {
       "@semantic-release/exec",
       {
         prepareCmd:
-          'sed -i "s/0.0.0/${nextRelease.version}/g" deno.publish.json && rm deno.json && mv deno.publish.json deno.json &&  cat deno.json',
+            'sed -i -E "s/\\"[0-9]+\\.[0-9]+\\.[0-9]+\\"/\\"${nextRelease.version}\\"/g" deno.json && cat deno.json',
         publishCmd: "npx jsr publish --allow-dirty",
       },
     ],
