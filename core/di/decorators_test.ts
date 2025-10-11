e9b09559cea9e1247fa819a963892611fafa02d0
Ziv Perry <ziv.perry@zoominfo.com>
2025-10-11 16:21:43 +0300

    feat(core): tests completed

--- core/di/decorators_test.ts
+++ core/di/decorators_test.ts
@@ -179,3 +179,158 @@ test("Module with Providers and Imports decorators should merge", () => {
     providers: [Service1, Service2],
   });
 });
+
+test("Middlewares decorator on method should add middlewares metadata", () => {
+  const middleware1 = () => {};
+  const middleware2 = () => {};
+  class TestClass {
+    @Middlewares(middleware1, middleware2)
+    testMethod() {}
+  }
+  expect(readMetadataObject(TestClass.prototype.testMethod)).toEqual({
+    middlewares: [middleware1, middleware2],
+  });
+});
+
+test("Catch decorator on method should add errorHandler metadata", () => {
+  const errorHandler = () => {};
+  class TestClass {
+    @Catch(errorHandler)
+    testMethod() {}
+  }
+  expect(readMetadataObject(TestClass.prototype.testMethod)).toEqual({
+    errorHandler,
+  });
+});
+
+test("Multiple Middlewares decorators on method should merge", () => {
+  const middleware1 = () => {};
+  const middleware2 = () => {};
+  const middleware3 = () => {};
+  class TestClass {
+    @Middlewares(middleware1)
+    @Middlewares(middleware2, middleware3)
+    testMethod() {}
+  }
+  // Decorators apply bottom-up, so middleware2 and middleware3 are applied first
+  expect(readMetadataObject(TestClass.prototype.testMethod)).toEqual({
+    middlewares: [middleware2, middleware3, middleware1],
+  });
+});
+
+test("Middlewares and Catch decorators on method should merge", () => {
+  const middleware = () => {};
+  const errorHandler = () => {};
+  class TestClass {
+    @Middlewares(middleware)
+    @Catch(errorHandler)
+    testMethod() {}
+  }
+  expect(readMetadataObject(TestClass.prototype.testMethod)).toEqual({
+    middlewares: [middleware],
+    errorHandler,
+  });
+});
+
+test("Method with both class-level and method-level Middlewares should be independent", () => {
+  const classMiddleware = () => {};
+  const methodMiddleware = () => {};
+  @Middlewares(classMiddleware)
+  class TestClass {
+    @Middlewares(methodMiddleware)
+    testMethod() {}
+  }
+  // Class-level metadata
+  expect(readMetadataObject(TestClass)).toEqual({
+    middlewares: [classMiddleware],
+  });
+  // Method-level metadata should be independent
+  expect(readMetadataObject(TestClass.prototype.testMethod)).toEqual({
+    middlewares: [methodMiddleware],
+  });
+});
+
+test("Method with both class-level and method-level Catch should be independent", () => {
+  const classErrorHandler = () => {};
+  const methodErrorHandler = () => {};
+  @Catch(classErrorHandler)
+  class TestClass {
+    @Catch(methodErrorHandler)
+    testMethod() {}
+  }
+  // Class-level metadata
+  expect(readMetadataObject(TestClass)).toEqual({
+    errorHandler: classErrorHandler,
+  });
+  // Method-level metadata should be independent
+  expect(readMetadataObject(TestClass.prototype.testMethod)).toEqual({
+    errorHandler: methodErrorHandler,
+  });
+});
+
+test("Multiple methods with different Middlewares should have independent metadata", () => {
+  const middleware1 = () => {};
+  const middleware2 = () => {};
+  class TestClass {
+    @Middlewares(middleware1)
+    method1() {}
+
+    @Middlewares(middleware2)
+    method2() {}
+  }
+  expect(readMetadataObject(TestClass.prototype.method1)).toEqual({
+    middlewares: [middleware1],
+  });
+  expect(readMetadataObject(TestClass.prototype.method2)).toEqual({
+    middlewares: [middleware2],
+  });
+});
+
+test("Middlewares decorator on method should work with class middlewares", () => {
+  class AuthMiddleware {}
+  const loggingMiddleware = () => {};
+  const cacheMiddleware = () => {};
+
+  @Controller("/api")
+  @Middlewares(AuthMiddleware, loggingMiddleware)
+  class ApiController {
+    @Middlewares(cacheMiddleware)
+    getUsers() {}
+  }
+
+  // Class should have its middlewares
+  expect(readMetadataObject(ApiController)).toEqual({
+    route: "/api",
+    isGateway: true,
+    middlewares: [AuthMiddleware, loggingMiddleware],
+  });
+
+  // Method should have its own middlewares
+  expect(readMetadataObject(ApiController.prototype.getUsers)).toEqual({
+    middlewares: [cacheMiddleware],
+  });
+});
+
+test("Catch decorator should work with class error handlers", () => {
+  const classErrorHandler = () => {};
+  const methodErrorHandler = () => {};
+
+  @Controller("/api")
+  @Catch(classErrorHandler)
+  class ApiController {
+    @Catch(methodErrorHandler)
+    riskyOperation() {}
+  }
+
+  // Class should have its error handler
+  expect(readMetadataObject(ApiController)).toEqual({
+    route: "/api",
+    isGateway: true,
+    errorHandler: classErrorHandler,
+  });
+
+  // Method should have its own error handler
+  expect(readMetadataObject(ApiController.prototype.riskyOperation)).toEqual({
+    errorHandler: methodErrorHandler,
+  });
+});
