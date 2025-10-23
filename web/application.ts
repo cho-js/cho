52b88edc40b7deb568924f43406872b2a83a7356
Ziv Perry <ziv.perry@zoominfo.com>
2025-10-23 16:32:01 +0300

    feat(web): align versions

--- web/application.ts
+++ web/application.ts
@@ -1,6 +1,7 @@
 import type { Ctr } from "@chojs/core/meta";
 import type { CompiledModule } from "@chojs/core/application";
 import { Compiler } from "@chojs/core/application";
+import { Injector } from "@chojs/core/di";
 import type { ChoWebAdapter } from "./adapter.ts";
 import { Linker } from "./linker.ts";
 import {
@@ -38,20 +39,48 @@ export class Application<AppRef> {
     const compiled = await new Compiler().compile(graphBuilder(feature));
     await onModuleInit(compiled);
 
-    const linked = new Linker(adapter as ChoWebAdapter).link(compiled);
+    const linked: T = new Linker(adapter as ChoWebAdapter).link(compiled);
     await onModuleActivate(compiled, linked);
 
+    const injector = await Injector.get(feature);
+
     return new Application<T>(
-      compiled as CompiledModule,
-      linked as T,
+      compiled,
+      injector,
+      linked,
       adapter as ChoWebAdapter,
     );
   }
 
   constructor(
     readonly instance: CompiledModule,
+    readonly injector: Injector,
     readonly appRef: AppRef,
     readonly adapter: ChoWebAdapter,
   ) {
+  }
+
+  /**
+   * Select a compiled module by its constructor.
+   * @param ctr
+   */
+  select(ctr: Ctr): CompiledModule {
+    for (const cm of this.it()) {
+      if (cm.ctr === ctr) {
+        return cm;
+      }
+    }
+    throw new Error(`Module "${ctr.name}" not found in application`);
+  }
+
+  resolve<T>(ctr: Ctr<T>): T {
+  }
+
+  // iterate over all modules in the application
+  *it(cm = this.instance): Generator<CompiledModule> {
+    yield cm;
+    for (const im of this.instance.imports) {
+      yield* this.it(im);
+    }
   }
 }
